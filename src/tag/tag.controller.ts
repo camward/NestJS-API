5b08e2323dca03cd3cec3b86b7964b5c8f6e690f
alex <camward@yandex.ru>
2022-06-15 20:31:43 +0400

    сервис тегов

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,9 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
+import { TagService } from './tag.service';
 
 @Controller('tags')
 export class TagController {
+  constructor(private readonly tagService: TagService) {}
+
   @Get()
   findAll(): string[] {
-    return ['dragons', 'coffee'];
+    return this.tagService.findAll();
   }
 }
