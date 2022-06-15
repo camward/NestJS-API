4f3c68214145a33b1a0a292d38d07767f7b6e0a8
alex <camward@yandex.ru>
2022-06-15 20:37:24 +0400

    полученте тегов из БД

--- src/tag/tag.module.ts
+++ src/tag/tag.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { TagController } from './tag.controller';
-import {TagService} from './tag.service';
+import { TagEntity } from './tag.entity';
+import { TagService } from './tag.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([TagEntity])],
   controllers: [TagController],
-  providers: [TagService]
+  providers: [TagService],
 })
 export class TagModule {}
