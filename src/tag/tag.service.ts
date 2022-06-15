29370e7c54491384311504de491ce8d65aa373f1
alex <camward@yandex.ru>
2022-06-15 20:32:21 +0400

    абсолютные пути

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TagService {
   findAll(): string[] {
-    return ['dragons', 'coffee', 'weather'];
+    return ['dragons', 'coffee'];
   }
 }
