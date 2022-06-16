efae999781f19a67030859af74ea821a704a98b5
alex <camward@yandex.ru>
2022-06-16 19:24:51 +0400

    миграции

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,11 @@ const config: ConnectionOptions = {
   password: '123',
   database: 'mediumclone',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+  cli: {
+    migrationsDir: 'src/migrations',
+  },
 };
 
 export default config;
