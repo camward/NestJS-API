a545cd84324a7166c82df72d60bef6abb4cc1389
alex <camward@yandex.ru>
2022-06-18 11:25:02 +0400

    модуль профиля

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import ormconfig from '@app/ormconfig';
 import { UserModule } from '@app/user/user.module';
 import { AuthMiddleware } from './user/middlewares/auth.middleware';
 import { ArticleModule } from './article/article.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
   imports: [
@@ -14,6 +15,7 @@ import { ArticleModule } from './article/article.module';
     TagModule,
     UserModule,
     ArticleModule,
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/profile/profile.service.ts
@@ -0,0 +1,34 @@
+import { UserEntity } from '@app/user/user.entity';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { ProfileType } from './types/profile.type';
+import { ProfileResponseInterface } from './types/profileResponse.interface';
+
+@Injectable()
+export class ProfileService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async getProfile(
+    currentUserId: number,
+    profileUsername: string,
+  ): Promise<ProfileType> {
+    const user = await this.userRepository.findOne({
+      username: profileUsername,
+    });
+
+    if (!user) {
+      throw new HttpException('Profile does not exist', HttpStatus.NOT_FOUND);
+    }
+
+    return { ...user, following: false };
+  }
+
+  buildProfileResponse(profile: ProfileType): ProfileResponseInterface {
+    delete profile.email;
+    return { profile };
+  }
+}
